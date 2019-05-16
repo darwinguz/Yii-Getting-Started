c90072a788f5b0e83f6a787232e81c6b2bcb9b6b
Darwin Guzmán <dga_guz@hotmail.es>
2019-05-16 15:13:46 -0500

    Hola Mundo creado

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -125,4 +125,10 @@ class SiteController extends Controller
     {
         return $this->render('about');
     }
+
+    public function actionSay($message = 'Hola')
+    {
+        return $this->render('say', ['message' => $message]);
+    }
+
 }
